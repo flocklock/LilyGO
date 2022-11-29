5bb0fa80ae1292748a0752eca62487323a37f53f
moteele <daniel.rychly@posteo.cz>
2022-11-29 07:25:36 +0100

    backup

--- product/src/main_backup.cpp
+++ product/src/main_backup.cpp
@@ -1,3 +1,5 @@
+
+/*
 #include <Arduino.h>
 #include <acc.hpp>
 #include <utils.hpp>
@@ -9,7 +11,7 @@ bool readGnssFlag = true;
 hw_timer_t *accTimer = NULL;
 hw_timer_t *gnssTimer = NULL;
 unsigned long lastGnssCheck = 0;
-unsigned long lastFotaCheck = 1;
+unsigned long lastFotaCheck = 0;
 
 
 //#include <location.hpp>
@@ -112,17 +114,8 @@ void loop() {
             SerialAT.write(SerialMon.read());
         }
     }
-
-
-  esp_sleep_enable_timer_wakeup(10 * uS_TO_S_FACTOR);
-  esp_light_sleep_start();
-  for (int i = 0; i < 31; i++) {
-    e[i] = accel.readAccData(ADXL345_REG_DATAX0);
-    D(Serial.println(String("reading data: ") + String(e[i].X, 2));)
-  }
   
 }
-*/
 
 #include <fotaHeaders.h>
 
@@ -153,14 +146,6 @@ void setup()
   setupFOTAGSM();
   modem.sendAT("+CSCLK=1");
   modem.sendAT("+CNETLIGHT=0");
-  delay(100);
-  digitalWrite(PIN_DTR, HIGH);
-  digitalWrite(LED_PIN, HIGH);
-  //esp_sleep_enable_timer_wakeup(20 * uS_TO_S_FACTOR);
-  D(SerialMon.flush();)
-  //esp_light_sleep_start();
-  digitalWrite(LED_PIN, LOW);
-
 
   SerialMon.begin(115200);
   //D(Serial.begin(115200);)
@@ -172,118 +157,57 @@ void setup()
   accel.writeRegister(ADXL345_REG_FIFO_CTL, 0b10000000);
   accel.writeRegister(ADXL345_REG_INT_ENABLE, 0b10000000);
   accel.writeRegister(ADXL345_REG_INT_MAP, 0b00000000);
-
+/*
   accTimer = timerBegin(0, 80, true);
   timerAttachInterrupt(accTimer, onAccTimer, true);
   timerAlarmWrite(accTimer, 10 * uS_TO_S_FACTOR, true);
   timerAlarmEnable(accTimer);
-
-  gnssTimer = timerBegin(1, 80, true);
-  timerAttachInterrupt(gnssTimer, onGnssTimer, true);
-  timerAlarmWrite(gnssTimer, 20 * uS_TO_S_FACTOR, true);
-  timerAlarmEnable(gnssTimer);
+  
 
   delay(1000);
   setCpuFrequencyMhz(80);
   delay(1000);
+  enableGPS();
 }
-
+void blink(int interval, int duration){
+  int time = millis();
+  while(millis() - time < duration) {
+  digitalWrite(LED_PIN, LOW);
+  delay(interval);
+  digitalWrite(LED_PIN, HIGH);
+  delay(interval);
+  }
+}
 void loop()
 {
-  
-  D(SerialMon.println(String("acc reading"));)
-    for (int i = 0; i < 32; i++) {
-    e[i] = accel.readAccData(ADXL345_REG_DATAX0);
-    }
-    D(SerialMon.println(String("time: ") + String(millis()) );)
-    for(int i = 0; i < 32; i++) {
-      D(SerialMon.println(String("acc data: ") + String(e[i].X, 2));)
-    }
-
-  if(!lastGnssCheck || millis() - lastGnssCheck > 20000) {
-    digitalWrite(PIN_DTR, LOW);
-    if (modem.testAT()) {
-        D(Serial.println("Modem ok");)
-        //modem.sendAT("+CSCLK=0");
-        delay(10);
-        modem.sendAT("+CGNSPWR=1");
-        if (modem.waitResponse(10000L) != 1) {
-        DBG(" CGNSPWR=1 false ");
-        }
-        modem.sendAT("+SGPIO=0,4,1,1");
-        if (modem.waitResponse(10000L) != 1) {
-        DBG(" SGPIO=0,4,1,1 false ");
-        }
-
-        float lat,  lon;
-        while (1) {
-          //modem.sendAT(GF("+CGNSINF"));
-          modem.sendAT("+CGNSINF");
-          if (modem.waitResponse(10000L, GF(GSM_NL "+CGNSINF:")) != 1) {
-            continue;
-    
-          String res = stream.readStringUntil('\n');
-          modem.waitResponse();
-          res.trim();
-          if (modem.getGPS(&lat, &lon)) {
-            D(Serial.println("The location has been locked, the latitude and longitude are:");)
-            D(Serial.print("latitude:"); Serial.println(lat);)
-            D(Serial.print("longitude:"); Serial.println(lon);)
-            break;
-        }
-        D(Serial.println("No fix");)
-        digitalWrite(LED_PIN, HIGH);
-        delay(1000);
-        digitalWrite(LED_PIN, LOW);
-        delay(1000);
-      }
-      modem.sendAT("+CGNSPWR=0");
-      if (modem.waitResponse(10000L) != 1) {
-        DBG(" CGNSPWR=0 false ");
-        }
-      modem.sendAT("+SGPIO=0,4,1,0");
-      if (modem.waitResponse(10000L) != 1) {
-        DBG(" SGPIO=0,4,1,01 false ");
-        }
-      delay(100);
-      modem.sendAT("+CSCLK=1");
-      delay(10);
-      digitalWrite(PIN_DTR, HIGH);
-    }
-    else {
-      D(Serial.println("Modem not responding");)
-    }
-    lastGnssCheck = millis();
-    digitalWrite(PIN_DTR, HIGH);
-  }
-  if(!lastFotaCheck || millis() - lastFotaCheck > 3600000) {
-    digitalWrite(PIN_DTR, LOW);
-    bool updatedNeeded = fota.execHTTPcheck();
-  if (updatedNeeded)
-  {
-    D(SerialMon.println("Got new update");)
-    fota.execOTA();    
-  }
-  else
-  {    
-    D(SerialMon.println("Already up to date. No need to update");)
-  }
-  lastFotaCheck = millis();
-  modem.sendAT("+CSCLK=1");
+  disableGPS();
+  digitalWrite(PIN_DTR, LOW);
+  blink(2000, 10000);
+  //modem.sendAT("+CSCLK=1");
+  delay(10);
+  //modem.sendAT("+CSCLK=?");
   delay(10);
   digitalWrite(PIN_DTR, HIGH);
-  }
-  digitalWrite(PIN_DTR, LOW);
-  modem.sendAT("+CSCLK=0");
-  delay(20);
-  modem.sendAT("+CSCLK=1");
-  delay(20);
+  //modem.sendAT("+CSCLK=?");
+  blink(1000, 10000);
+  //digitalWrite(PIN_DTR, LOW);
+
+  esp_sleep_enable_timer_wakeup(20 * uS_TO_S_FACTOR);
   digitalWrite(LED_PIN, LOW);
-  digitalWrite(PIN_DTR, HIGH);
-  esp_sleep_enable_timer_wakeup(10 * uS_TO_S_FACTOR);
+  esp_light_sleep_start();
+  disableGPS();
+/*
+  esp_sleep_enable_timer_wakeup(20 * uS_TO_S_FACTOR);
+  gpio_hold_en(GPIO_NUM_25);
+  gpio_hold_en(GPIO_NUM_12);
+  delay(1);
   D(SerialMon.flush();)
   esp_light_sleep_start();
+  gpio_hold_dis(GPIO_NUM_25);
+  gpio_hold_dis(GPIO_NUM_12);
   digitalWrite(LED_PIN, HIGH);
   delay(10);
+  
 
 }
+*/
